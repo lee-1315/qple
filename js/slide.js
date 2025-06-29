c664df08363ec86514c8e3bd489d62a1fa279962
lee-1315 <webomakase@gmail.com>
2025-06-30 00:12:56 +0900

    docs style: 서브페이지 슬라이더 완료

--- js/slide.js
+++ js/slide.js
@@ -1,14 +1,14 @@
-let swiper = new Swiper('.swiper', {
+let swiper = new Swiper(".swiper", {
   slidesPerView: 4,
   grid: {
     rows: 2,
-    fill: 'row',
+    fill: "row",
   },
   spaceBetween: 20,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   /* pagination: {
