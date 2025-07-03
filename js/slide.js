bd97ddf1b78b453ac2f4809af4bb7354024abfc6
lee-1315 <webomakase@gmail.com>
2025-07-03 18:14:17 +0900

    docs style: 메인페이지 반응형 작업

--- js/slide.js
+++ js/slide.js
@@ -12,10 +12,45 @@ let swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  /* pagination: {
-    el: ".swiper-pagination",
+  pagination: {
+    el: '.swiper-pagination',
     clickable: true,
     mousewheel: true,
     keyboard: true,
+  },
+
+  /* 반응형 설정 */
+  /* breakpoints: {
+    320: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+      spaceBetween: 20,
+      navigation: false,
+    },
+
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+      spaceBetween: 20,
+      navigation: false,
+    },
+
+    1024: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+      spaceBetween: 20,
+    },
   }, */
 });
