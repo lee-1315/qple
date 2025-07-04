9f5452a928aee85597fab3f86543904b3a02ee31
lee-1315 <webomakase@gmail.com>
2025-07-05 01:12:37 +0900

    docs style: 메인페이지 반응형 완료

--- js/slide.js
+++ js/slide.js
@@ -1,45 +1,45 @@
-let swiper = new Swiper('.swiper', {
+let swiper = new Swiper(".swiper", {
   slidesPerView: 4,
   slidesPerGroup: 4,
   grid: {
     rows: 2,
-    fill: 'row',
+    fill: "row",
   },
   spaceBetween: 20,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
     mousewheel: true,
     keyboard: true,
   },
 
   /* 반응형 설정 */
-  /* breakpoints: {
+  breakpoints: {
     320: {
       slidesPerView: 2,
       slidesPerGroup: 2,
       grid: {
         rows: 2,
-        fill: 'row',
+        fill: "row",
       },
-      spaceBetween: 20,
+      spaceBetween: 10,
       navigation: false,
     },
 
     768: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
       grid: {
         rows: 2,
-        fill: 'row',
+        fill: "row",
       },
-      spaceBetween: 20,
+      spaceBetween: 15,
       navigation: false,
     },
 
@@ -48,9 +48,9 @@ let swiper = new Swiper('.swiper', {
       slidesPerGroup: 4,
       grid: {
         rows: 2,
-        fill: 'row',
+        fill: "row",
       },
       spaceBetween: 20,
     },
-  }, */
+  },
 });
